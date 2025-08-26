ad7bbdf329a40413a0912dc43cca16cedc773fe9
Deb Nath <atanudebnath67@gmail.com>
2025-08-26 23:55:58 +0530

    add new

---
+++ Medium/57. Insert Interval.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, arr: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        arr.append(newInterval)
+        arr.sort()
+
+        ans = [arr[0]]
+        for i in range(1, len(arr)):
+        
+            if arr[i][0] <= ans[-1][1]:
+                ans[-1][1] = max(ans[-1][1], arr[i][1])
+            else:
+                ans.append(arr[i])
+
+        return ans
+        
