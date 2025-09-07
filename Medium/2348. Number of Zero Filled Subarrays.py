4c6ea8104f4e16bba514f59c5b01a6cdf46a5f77
Deb Nath <atanudebnath67@gmail.com>
2025-09-07 09:21:08 +0530

    add new

---
+++ Medium/2348. Number of Zero Filled Subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def zeroFilledSubarray(self, nums):
+        count = 0
+        zero_streak = 0
+
+        for num in nums:
+            if num == 0:
+                zero_streak += 1
+                count += zero_streak  
+            else:
+                zero_streak = 0 
+
+        return count
